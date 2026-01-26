63ad730c583f5daa377c6af68a3c37733fd2f9e6
hanson92828 <hanson92828@gmail.com>
2026-01-26 11:38:46 +0800

    CI/CD test

--- src/caculator/caculate.py
+++ src/caculator/caculate.py
@@ -1,5 +1,25 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
 def add_func(a: float,b: float) -> float:
     return a+b
 
 def subs_func(a: float,b: float) -> float:
     return a-b
+''' ------------------------------------- '''
+@app.get("/")
+def home():
+    return {"status": "Online", "message": "這是簡易計算機 API"}
+
+@app.get("/add")
+def calculate_add(a: float, b: float):
+    # 調用你的 add_func
+    result = add_func(a, b)
+    return {"operation": "addition", "a": a, "b": b, "result": result}
+
+@app.get("/subs")
+def calculate_subs(a: float, b: float):
+    # 調用你的 subs_func
+    result = subs_func(a, b)
+    return {"operation": "subtraction", "a": a, "b": b, "result": result}
