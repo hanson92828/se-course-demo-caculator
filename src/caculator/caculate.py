e7593ecccbb42c8728cc66a54a3dc7d134b3d0c3
hanson92828 <hanson92828@gmail.com>
2026-01-27 11:40:08 +0800

    file edit: caculate.py

--- src/caculator/caculate.py
+++ src/caculator/caculate.py
@@ -18,18 +18,18 @@ def home():
 
 @app.get("/add")
 def calculate_add(a: float, b: float):
-    # 調用你的 add_func
+    # add_func
     result = add_func(a, b)
     return {"operation": "addition", "a": a, "b": b, "result": result}
 
 @app.get("/subs")
 def calculate_subs(a: float, b: float):
-    # 調用你的 subs_func
+    # subs_func
     result = subs_func(a, b)
     return {"operation": "subtraction", "a": a, "b": b, "result": result}
 
 @app.get("/mul")
 def calculate_multi(a: float, b: float):
-    # 調用你的 multi_func
+    # multi_func
     result = multi_func(a, b)
     return {"operation": "multiplication", "a": a, "b": b, "result": result}
