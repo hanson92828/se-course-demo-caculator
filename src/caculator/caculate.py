4eb90f8cc249f884d6911657788a9409ce64af3e
hanson92828 <hanson92828@gmail.com>
2026-01-27 11:21:09 +0800

    add func: multi_func

--- src/caculator/caculate.py
+++ src/caculator/caculate.py
@@ -7,6 +7,10 @@ def add_func(a: float,b: float) -> float:
 
 def subs_func(a: float,b: float) -> float:
     return a-b
+
+def multi_func(a: float,b: float) -> float:
+    return a*b
+ 
 ''' ------------------------------------- '''
 @app.get("/")
 def home():
@@ -23,3 +27,9 @@ def calculate_subs(a: float, b: float):
     # 調用你的 subs_func
     result = subs_func(a, b)
     return {"operation": "subtraction", "a": a, "b": b, "result": result}
+
+@app.get("/mul")
+def calculate_multi(a: float, b: float):
+    # 調用你的 multi_func
+    result = multi_func(a, b)
+    return {"operation": "multiplication", "a": a, "b": b, "result": result}
--- test/unit/test_caculate.py
+++ test/unit/test_caculate.py
@@ -1,6 +1,6 @@
 import pytest
-from caculator.caculate import add_func
-from caculator.caculate import subs_func
+from caculator.caculate import add_func, subs_func, multi_func
+
 
 def test_add_func():
     assert add_func(1, 2) == 3
@@ -11,3 +11,8 @@ def test_subs_func():
     assert subs_func(2, 1) == 1
     assert subs_func(-1, 1) == -2
     assert subs_func(0, 0) == 0 
+    
+def test_multi_func():
+    assert multi_func(2, 1) == 2
+    assert multi_func(-1, 1) == -1
+    assert multi_func(0, 0) == 0
